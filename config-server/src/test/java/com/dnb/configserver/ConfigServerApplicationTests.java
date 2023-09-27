d281e6c75ec81989d2b0d632ce3e2936987e3be7
sathvika-rao <sathvikarao09@gmail.com>
2023-09-27 14:14:08 +0530

    Creating config-server

---
+++ config-server/src/test/java/com/dnb/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnb.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
